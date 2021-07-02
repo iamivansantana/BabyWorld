2e3c5c5b3ceb6a94a538b3da3f7322cf1a47ac42
unknown <iam.ivansantana@gmail.com>
2021-07-01 23:46:40 -0500

    making appsMenuToggle

--- src/components/coverPage/BWCover.js
+++ src/components/coverPage/BWCover.js
@@ -5,8 +5,17 @@ const BWCover = () => {
     return (
         <>
             <div className="vh-100 bw-cover">
-                <div className="bw-cover-content">
-                    <p>Hola tu</p>
+                <div className="bw-cover-content-blur">
+                    <div className="bw-cover-contenedor">
+                        <div className="bw-cover-content">Content</div>
+                        <div className="bw-cover-dow">
+                            <div className="bw-cover-content-grid">
+                                <div className="white"></div>
+                                <div className="arrow"></div>
+                                <div className="links"></div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
--- src/components/navBar/BWNavBar.js
+++ src/components/navBar/BWNavBar.js
@@ -1,15 +1,63 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './BWNavBar.css';
 
 const BWNavBar = () => {
+
+  useEffect(() => {
+    
+    const navToggle = document.querySelector(".toggle");
+    const navMenu = document.querySelector(".nav-menu");
+    const navMenuBackground = document.querySelector(".nav-menu-backgroud");
+    
+    navToggle.addEventListener("click",()=>{
+      navMenu.classList.toggle("nav-menu-active");
+      navMenuBackground.classList.toggle("nav-menu-backgroud-active")
+    });
+    navMenuBackground.addEventListener("click",()=>{
+      navMenu.classList.toggle("nav-menu-active");
+      navMenuBackground.classList.toggle("nav-menu-backgroud-active")
+    });
+
+
+    
+  });
+
     return (
         <>
           <div className="navBarContent">
             <div className="flex">
-              <div className="logo">
-                <img src="\assets\BW-Resources\tittle.svg" alt="BabyWorldLogo"/>
-              </div>
-              <div className="menu"><p>HOME--ABOUT--CONTACT--...--LOGIN/REGISTER</p></div>
+
+              <button className="toggle">
+                  <img className="bars" src="\assets\BW-Resources\burgerMenu.svg" alt="Menu" />
+              </button>
+                <div className="logo">
+                  <img src="\assets\BW-Resources\tittle.svg" alt="BabyWorldLogo"/>
+                </div>
+                <button className="toggleApps">
+                  <img className="bars" src="\assets\BW-Resources\appsMenu.svg" alt="Menu" />
+              </button>
+
+                <nav className="">
+                  <div className="nav-menu-backgroud"/>
+                  <ul style={{margin:'0',padding:'0'}}>
+                    <div className="nav-menu">
+                        {/*  eslint-disable-next-line */}
+                        <li className="item"><a className="bw-link bw-link-active" href="#">HOME</a></li>
+                        {/*  eslint-disable-next-line */}
+                        <li className="item"><a className="bw-link" href="#">ABOUT</a></li>
+                        {/* eslint-disable-next-line */}
+                        <li className="item"><a className="bw-link" href="#">CONTACT</a></li>
+                        {/* eslint-disable-next-line */}
+                        <li className="item slash"><a className="bw-link" href="#"><img className="appsMenu" src="\assets\BW-Resources\appsMenu.svg" alt="Menu" /></a></li>
+                        {/* eslint-disable-next-line */}
+                        <li className="item "><a className="bw-link" href="#">LOGIN</a></li>
+                        <li className=" slash ">/</li>
+                        {/* eslint-disable-next-line */}
+                        <li className="item"><a className="bw-link" href="#">REGISTER</a></li>
+                      
+                    </div>
+                  </ul>
+                </nav>
             </div>
           </div>
         </>
